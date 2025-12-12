f010ae8d58f7ed23e6f37b293355bb44cb2d18e9
lizzyjoo <jejviolinist@gmail.com>
2025-12-12 01:50:29 -0500

    add analytics

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+import { Analytics } from "@vercel/analytics/next";
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
